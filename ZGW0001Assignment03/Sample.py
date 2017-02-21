51c55ca7c4448b5268640c88b296f49edbdf0d4c
zackweld <zackweld@gmail.com>
2017-02-21 09:07:45 -0600

    Red Light |

--- ZGW0001Assignment03/Sample.py
+++ ZGW0001Assignment03/Sample.py
@@ -17,7 +17,7 @@ class Sample(object):
     def getN(self):
         return self.n
 
-    
+
     def p(self, t=None, tails=1):
         functionName = "Sample.p: "
         if(t == None):
@@ -26,24 +26,24 @@ class Sample(object):
             raise ValueError(functionName + "invalid t")
         if(t < 0.0):
             raise ValueError(functionName + "invalid t")
-        
+
         if(not(isinstance(tails, int))):
             raise ValueError(functionName + "invalid tails")
         if((tails != 1) & (tails != 2)):
             raise ValueError(functionName + "invalid tails")
-        
+
         constant = self.calculateConstant(self.n)
         integration = self.integrate(0.0, t, self.n, self.f)
         if(tails == 1):
             result = constant * integration + 0.5
         else:
             result = constant * integration * 2
-            
+
         if(result > 1.0):
             raise ValueError(functionName + "result > 1.0")
-        
+
         return result
-        
+
 # internal methods
     def gamma(self, x):
         if(x == 1):
@@ -51,14 +51,14 @@ class Sample(object):
         if(x == 0.5):
             return math.sqrt(math.pi)
         return (x - 1) * self.gamma(x - 1)
-    
+
     def calculateConstant(self, n):
         n = float(n)
         numerator = self.gamma((n + 1.0) / 2.0)
         denominator = self.gamma(n / 2.0) * math.sqrt(n * math.pi)
         result = numerator / denominator
         return result
-    
+
     def f(self, u, n):
         # n = float(n)
         # base = (1 + (u ** 2) / n)
@@ -97,4 +97,3 @@ class Sample(object):
         # return simpsonNew
 
         pass
-
