23c0213b15974272c38eb9660ef3ee04e764120c
zackweld <zackweld@gmail.com>
2017-03-20 22:44:43 -0500

    Red Light |

--- ZGW0001Assignment04/dispatchTest.py
+++ ZGW0001Assignment04/dispatchTest.py
@@ -23,32 +23,31 @@ class DispatchTest(unittest.TestCase):
         self.assertDictEqual(expected_result, dispatch.dispatch({'temperature': '72', 'op': 'Wrong'}))
 
 
-    # def test100_050_DispatchExecutesAdjust(self):
-    #     passed = 0
-    #     values = {'temperature': '72', 'op': 'adjust'}
-    #     #Validate parm
-    #     if(values == None):
-    #         return {'error': 'parameter is missing'}
-    #     if(not(isinstance(values,dict))):
-    #         return {'error': 'parameter is not a dictionary'}
-    #     if (not('op' in values)):
-    #         values['error'] = 'no op is specified'
-    #         return values
-    #
-    #     #Perform designated function
-    #     if(values['op'] == 'adjust'):
-    #         passed = 1
-    #         return values    #<-------------- replace this with your implementation
-    #     elif(values['op'] == 'predict'):
-    #         return values    #This calculation is stubbed out
-    #     elif(values['op'] == 'correct'):
-    #         return values    #This calculation is stubbed out
-    #     elif(values['op'] == 'locate'):
-    #         return values    #This calculation is stubbed out
-    #     else:
-    #         del values['op']
-    #         values['error'] = 'op is not a legal operation'
-    #         return values
-    #
-    #     self.assertEquals(passed, 1)
+    def test100_050_DispatchExecutesAdjust(self):
+        passed = 0
+        values = {'temperature': '72', 'op': 'adjust'}
+        #Validate parm
+        if(values == None):
+            return {'error': 'parameter is missing'}
+        if(not(isinstance(values,dict))):
+            return {'error': 'parameter is not a dictionary'}
+        if (not('op' in values)):
+            values['error'] = 'no op is specified'
+            return values
+
+        #Perform designated function
+        if(values['op'] == 'adjust'):
+            passed = 1
+            return values    #<-------------- replace this with your implementation
+        elif(values['op'] == 'predict'):
+            return values    #This calculation is stubbed out
+        elif(values['op'] == 'correct'):
+            return values    #This calculation is stubbed out
+        elif(values['op'] == 'locate'):
+            return values    #This calculation is stubbed out
+        else:
+            values['error'] = 'op is not a legal operation'
+            return values
+
+        self.assertEquals(passed, 1)
 
