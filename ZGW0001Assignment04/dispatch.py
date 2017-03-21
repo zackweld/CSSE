5ab231061a8db83f37bf98d1a4791d49512c0314
zackweld <zackweld@gmail.com>
2017-03-20 19:45:47 -0500

    Green Light |

---
+++ ZGW0001Assignment04/dispatch.py
@@ -0,0 +1,23 @@
+def dispatch(values=None):
+
+    #Validate parm
+    if(values == None):
+        return {'error': 'parameter is missing'}
+    if(not(isinstance(values,dict))):
+        return {'error': 'parameter is not a dictionary'}
+    if (not('op' in values)):
+        values['error'] = 'no op  is specified'
+        return values
+
+    #Perform designated function
+    if(values['op'] == 'adjust'):
+        return values    #<-------------- replace this with your implementation
+    elif(values['op'] == 'predict'):
+        return values    #This calculation is stubbed out
+    elif(values['op'] == 'correct'):
+        return values    #This calculation is stubbed out
+    elif(values['op'] == 'locate'):
+        return values    #This calculation is stubbed out
+    else:
+        values['error'] = 'op is not a legal operation'
+        return values
