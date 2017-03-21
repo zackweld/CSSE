8e88aa580f3a9905c16d781f78ef9561d4fece78
zackweld <zackweld@gmail.com>
2017-03-20 22:48:24 -0500

    Red Light |

--- ZGW0001Assignment04/dispatchTest.py
+++ ZGW0001Assignment04/dispatchTest.py
@@ -26,29 +26,29 @@ class DispatchTest(unittest.TestCase):
     def test100_050_DispatchExecutesAdjust(self):
         passed = "fail"
         values = {}
-        #Validate parm
-        if(values == None):
-            return {'error': 'parameter is missing'}
-        if(not(isinstance(values,dict))):
-            return {'error': 'parameter is not a dictionary'}
-        if (not('op' in values)):
-            values['error'] = 'no op is specified'
-            return values
+        # #Validate parm
+        # if(values == None):
+        #     return {'error': 'parameter is missing'}
+        # if(not(isinstance(values,dict))):
+        #     return {'error': 'parameter is not a dictionary'}
+        # if (not('op' in values)):
+        #     values['error'] = 'no op is specified'
+        #     return values
 
         #Perform designated function
         if(values['op'] == 'adjust'):
             passed = "pass"
-            return values    #<-------------- replace this with your implementation
+            # return values    #<-------------- replace this with your implementation
         elif(values['op'] == 'predict'):
-            return values    #This calculation is stubbed out
+            # return values    #This calculation is stubbed out
         elif(values['op'] == 'correct'):
-            return values    #This calculation is stubbed out
+            # return values    #This calculation is stubbed out
         elif(values['op'] == 'locate'):
-            return values    #This calculation is stubbed out
+            # return values    #This calculation is stubbed out
         else:
             del values['op']
             values['error'] = 'op is not a legal operation'
-            return values
+            # return values
 
         self.assertEquals(passed, "pass")
 
