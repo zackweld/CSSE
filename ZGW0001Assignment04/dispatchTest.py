22ac5909711632d3860a8eedc09fe456d71da82a
zackweld <zackweld@gmail.com>
2017-03-20 23:56:19 -0500

    Red Light |

--- ZGW0001Assignment04/dispatchTest.py
+++ ZGW0001Assignment04/dispatchTest.py
@@ -75,9 +75,15 @@ class DispatchTest(unittest.TestCase):
 
     def test200_020_AdjustObservationInvalid(self):
         expected_result = {'op': 'adjust', 'horizon': 'natural', 'observation': '91', 'error': 'observation is invalid'}
-        call = {'op': 'adjust', 'horizon': 'natural', 'observation': '91'}
-        call2 = {'op': 'adjust', 'horizon': 'natural', 'observation': '0'}
+        call = {'op': 'adjust', 'horizon': 'natural', 'observation': '91d00.0'}
+        call2 = {'op': 'adjust', 'horizon': 'natural', 'observation': '0d00.0'}
+        call3 = {'op': 'adjust', 'horizon': 'natural', 'observation': '45d-05.0'}
+        call4 = {'op': 'adjust', 'horizon': 'natural', 'observation': '0d61.0'}
+        call5 = {'op': 'adjust', 'horizon': 'natural', 'observation': '000.0'}
         self.assertDictEqual(expected_result, dispatch.dispatch(call))
         self.assertDictEqual(expected_result, dispatch.dispatch(call2))
+        self.assertDictEqual(expected_result, dispatch.dispatch(call3))
+        self.assertDictEqual(expected_result, dispatch.dispatch(call4))
+        self.assertDictEqual(expected_result, dispatch.dispatch(call5))
 
 
