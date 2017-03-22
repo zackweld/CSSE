f295031291633eeb35605b16857f68f8a2ceb7a8
zackweld <zackweld@gmail.com>
2017-03-22 07:59:37 -0500

    Refactor |

--- ZGW0001Assignment04/dispatchTest.py
+++ ZGW0001Assignment04/dispatchTest.py
@@ -165,14 +165,14 @@ class DispatchTest(unittest.TestCase):
         expected_result = {'observation': '30d1.5', 'height': '19.0', 'pressure': '1000',
                 'horizon': 'artificial', 'op': 'adjust', 'temperature': '85',
                            'altitude': '29d59.9'}
-        # self.assertDictEqual(expected_result, dispatch.dispatch(call))
+        self.assertDictEqual(expected_result, dispatch.dispatch(call))
 
         call2 = {'observation': '45d15.2', 'height': '6', 'pressure': '1010',
                 'horizon': 'natural', 'op': 'adjust', 'temperature': '71'}
         expected_result2 = {'observation': '45d15.2', 'height': '6', 'pressure': '1010',
                 'horizon': 'natural', 'op': 'adjust', 'temperature': '71',
                             'altitude': '45d11.9'}
-        # self.assertDictEqual(expected_result2, dispatch.dispatch(call2))
+        self.assertDictEqual(expected_result2, dispatch.dispatch(call2))
 
         call3 = {'observation': '42d0.0', 'op': 'adjust'}
         expected_result3 = {'observation': '42d0.0', 'op': 'adjust', 'altitude': '41d59.0'}
