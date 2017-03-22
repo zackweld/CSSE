03bcc0727a4e61ca81c609471b5a776da62485d3
zackweld <zackweld@gmail.com>
2017-03-21 22:43:11 -0500

    Green Light |

--- ZGW0001Assignment04/dispatchTest.py
+++ ZGW0001Assignment04/dispatchTest.py
@@ -158,28 +158,28 @@ class DispatchTest(unittest.TestCase):
 # Check Calculations
 #########################
 
-    # def test300_010_AdjustValidCalculation(self):
-    #     call = {'observation': '30d1.5', 'height': '19.0', 'pressure': '1000',
-    #             'horizon': 'artificial', 'op': 'adjust', 'temperature': '85'}
-    #     expected_result = {'observation': '30d1.5', 'height': '19.0', 'pressure': '1000',
-    #             'horizon': 'artificial', 'op': 'adjust', 'temperature': '85',
-    #                        'altitude': '29d59.9'}
-    #     self.assertDictEqual(expected_result, dispatch.dispatch(call))
-    #
-    #     call2 = {'observation': '45d15.2', 'height': '6', 'pressure': '1010',
-    #             'horizon': 'natural', 'op': 'adjust', 'temperature': '71'}
-    #     expected_result2 = {'observation': '45d15.2', 'height': '6', 'pressure': '1010',
-    #             'horizon': 'natural', 'op': 'adjust', 'temperature': '71',
-    #                         'altitude': '45d11.9'}
-    #     self.assertDictEqual(expected_result2, dispatch.dispatch(call2))
-    #
-    #     call3 = {'observation': '42d0.0', 'op': 'adjust'}
-    #     expected_result3 = {'observation': '42d0.0', 'op': 'adjust', 'altitude': '41d59.0'}
-    #     self.assertDictEqual(expected_result3, dispatch.dispatch(call3))
-    #
-    #     call4 = {'observation': '42d0.0', 'op': 'adjust', 'extraKey': 'ignore'}
-    #     expected_result4 = {'observation': '42d0.0', 'op': 'adjust', 'extraKey': 'ignore',
-    #                         'altitude': '41d59.0'}
+    def test300_010_AdjustValidCalculation(self):
+        call = {'observation': '30d1.5', 'height': '19.0', 'pressure': '1000',
+                'horizon': 'artificial', 'op': 'adjust', 'temperature': '85'}
+        expected_result = {'observation': '30d1.5', 'height': '19.0', 'pressure': '1000',
+                'horizon': 'artificial', 'op': 'adjust', 'temperature': '85',
+                           'altitude': '29d59.9'}
+        self.assertDictEqual(expected_result, dispatch.dispatch(call))
+
+        call2 = {'observation': '45d15.2', 'height': '6', 'pressure': '1010',
+                'horizon': 'natural', 'op': 'adjust', 'temperature': '71'}
+        expected_result2 = {'observation': '45d15.2', 'height': '6', 'pressure': '1010',
+                'horizon': 'natural', 'op': 'adjust', 'temperature': '71',
+                            'altitude': '45d11.9'}
+        self.assertDictEqual(expected_result2, dispatch.dispatch(call2))
+
+        call3 = {'observation': '42d0.0', 'op': 'adjust'}
+        expected_result3 = {'observation': '42d0.0', 'op': 'adjust', 'altitude': '41d59.0'}
+        self.assertDictEqual(expected_result3, dispatch.dispatch(call3))
+
+        call4 = {'observation': '42d0.0', 'op': 'adjust', 'extraKey': 'ignore'}
+        expected_result4 = {'observation': '42d0.0', 'op': 'adjust', 'extraKey': 'ignore',
+                            'altitude': '41d59.0'}
 
     def test300_020_ConvertToCelsius(self):
         expected_result = 29.4444
