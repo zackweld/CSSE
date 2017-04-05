f3c7b982246fdefa96a83b753ba78d2730000aa5
zackweld <zackweld@gmail.com>
2017-04-04 20:30:43 -0500

    Green Light |

--- Test/dispatchTest.py
+++ Test/dispatchTest.py
@@ -309,7 +309,6 @@ class DispatchTest(unittest.TestCase):
     def test400_110_PredictAngleEarthsRotation(self):
         total_seconds = int(timedelta(days=16, hours=03, minutes=15, seconds=42).total_seconds())
         self.assertEquals(total_seconds, 1394142)
-        # rotation = total_seconds / 86164.1 * 360
-        # rotation = degrees_to_minutes(rotation)
-        rotation = 0
+        rotation = total_seconds / 86164.1 * 360
+        rotation = degrees_to_minutes(rotation)
         self.assertEquals(rotation, '164d54.5')
